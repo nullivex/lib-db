e6f8eae98211b9f2c9f5e9d2e8afd15ae86997cb
root <root@ost.dev.eggpire.com>
2012-12-03 13:15:18 -0500

    new item personality system added catalog data manaer

--- db.php
+++ db.php
@@ -21,12 +21,19 @@
 
 class Db {
 
+	const EXCEPTIONS = false;
+	const NO_EXCEPTIONS = false;
+	const FLATTEN = true;
+	const NO_FLATTEN = false;
+
 	static $inst = false;
 
 	private $config;
 	private $pdo;
 	private $connected = false;
 	private $query_count = 0;
+	
+	public $debug = false;
 
 	public static function _get(){
 		if(self::$inst == false) self::$inst = new Db();
@@ -86,26 +93,54 @@ class Db {
 		static $inst = false;
 	}
 
-	public static function prepwhere($pairs=array(),$bool='AND'){
-		// $pairs is an assoc array of [column_name]=>[value] such as array('age'=>21,'email'=>'jdoe@example.org')
-		// $bool  can be any valid SQL WHERE clause boolean comparator (default: 'AND')
-		// returns an array, with members:
-		//     [0] <string> the resulting WHERE query; compiled for use with PDO::prepare including leading space (ready-to-use)
-		//     [n] <array>  the values array; ready for use with PDO::execute
+	// $pairs is an assoc array of [column_name]=>[value] [ex: array('age'=>21,'email'=>'jdoe@example.org')]
+	// $bool  can be either: (string) valid SQL WHERE clause boolean comparator [default: 'AND']
+	//                       (array)  array of strings like the above, to be used in order [ex: array('OR','AND','AND NOT')]
+	// returns an array, with members:
+	//     [0] <string> the resulting WHERE clause; compiled for use with PDO::prepare including leading space (ready-to-use)
+	//     [n] <array>  the values array; ready for use with PDO::execute
+	public static function prepwhere($pairs=array(),$bool='AND',$type='WHERE'){
 		if(!count($pairs)) return array('',null);
-		$cols = array();
-		$vals = array();
+		
+		//pad bools
+		if(is_array($bool)) foreach($bool as &$b) $b = '=? '.$b.' ';
+		else $bool = '=? '.$bool.' ';
+		//fill bools if not enough
+		if(is_array($bool) && ($ca = count($pairs) - ($cb = count($bool))) > 0){
+			$bool = array_merge($bool,array_fill($cb+1,$ca,$bool[($cb-1)]));
+			unset($ca,$cb);
+		}
+		//prepare clause
+		$clause = sprintf(' %s %s=?',$type,implodei($bool,self::escape(array_keys($pairs))));
+		return array_merge(array($clause),array_values($pairs));
+		
+		/*
+		//this is the old way
+		$cols = $vals = array();
 		foreach($pairs as $col=>$val){
-			$cols[] = $col.'=?';
+			$cols[] = self::escape($col).'=?';
+			// $cols[] = '`'.implode('`.`',explode('.',$col)).'`=?';
 			$vals[] = $val;
 		}
-		array_unshift($vals,' WHERE '.implode(' '.$bool.' ',$cols));
-		unset($cols);
+		if(is_string($bool))
+			array_unshift($vals,' WHERE '.implode(' '.$bool.' ',$cols));
+		else if(is_array($bool)){
+			$clause = ' WHERE';
+			$boolptr = 0;
+			$maxptr = count($cols) - 1;
+			for($c = 0; $c < $maxptr; $c++){
+				$clause .= ' '.$col.' '.$bool[$boolptr];
+				if(++$boolptr > $maxptr) $boolptr = $maxptr;
+			}
+			$clause .= ' '.$cols[$maxptr];
+			array_unshift($clause);
+		}
 		return $vals;
+		*/
 	}
-
-
+	
 	public function run($stmt,$params=array()){
+		if($this->debug) debug_dump($stmt,$params);
 		$query = $this->prepare($stmt);
 		$query->execute($params);
 		return $query;
@@ -116,7 +151,7 @@ class Db {
 		$stmt = sprintf(
 			'INSERT INTO `%s` (%s) VALUES (%s)'
 			,$table
-			,rtrim('`'.implode('`,`',array_keys($params)),'`,').'`'
+			,implodei(',',self::escape(array_keys($params)))
 			,rtrim(str_repeat('?,',count($params)),',')
 		);
 		$this->run($stmt,array_values($params));
@@ -127,49 +162,75 @@ class Db {
 		$stmt = sprintf(
 			'INSERT INTO `%s` (%s) VALUES (%s) ON DUPLICATE KEY UPDATE %s'
 			,$table
-			,rtrim('`'.implode('`,`',array_keys($params)),'`,').'`'
+			,implodei(',',self::escape(array_keys($params)))
 			,rtrim(str_repeat('?,',count($params)),',')
-			,rtrim('`'.implode('` = ?, `',array_keys($params)),', `').'` = ?'
+			,implodei('=?,',self::escape(array_keys($params)))
 		);
-		var_dump($stmt);
 		$this->run($stmt,array_merge(array_values($params),array_values($params)));
 		return $this->lastInsertId();
 	}
 
 	public function update($table,$primary_key,$primary_key_value=null,$params=array()){
-		if(!count($params)) throw new Exception('No data provided for update to: '.$table);
 		if(is_array($primary_key)){
-			$key_stmt = rtrim('`'.implode('` = ? AND `',array_keys($primary_key)),',AND `').'` = ?';
-			$params = array_merge(array_values($primary_key),$primary_key_value);
+			$key_stmt = implodei('=? AND ',self::escape(array_keys($primary_key)));
+			$params = $primary_key_value;
 		} else {
-			$key_stmt = $primary_key.' = ?';
+			$key_stmt = self::escape($primary_key).' =?';
 		}
+		if(!count($params)) throw new Exception('No data provided for update to: '.$table);
 		$stmt = sprintf(
-			'UPDATE `%s` SET %s WHERE `%s` = ?'
+			'UPDATE `%s` SET %s WHERE %s'
 			,$table
-			,rtrim('`'.implode('` = ?, `',array_keys($params)),',`').'` = ?'
-			,$primary_key
+			,implodei('=?, ',self::escape(array_keys($params))).'=?'
+			,$key_stmt
 		);
 		if(!is_array($primary_key)) $params[] = $primary_key_value;
+		else $params = array_merge($params,array_values($primary_key));
 		return $this->run($stmt,array_values($params));
 	}
 
-	public function fetch($stmt,$params=array(),$throw_exception=false,$except_code=null){
+	public function fetch($stmt,$params=array(),$throw_exception=false,$except_code=null,$flatten=false){
+		if(is_array($stmt)) list($stmt,$params) = $stmt;
 		$query = $this->run($stmt,$params);
 		$result = $query->fetch();
 		$query->closeCursor();
-		if(!$result && $throw_exception !== false) throw new Exception($throw_exception,$except_code);
+		if(((!is_array($result)) || (count($result)==0)) && $throw_exception !== false)
+			throw new Exception($throw_exception,$except_code);
+		if($flatten && is_array($result) && (count($result)>0) && (count(array_keys($result)) == 1)){
+			$col = array_shift(array_keys($result));
+			$result = $result[$col];
+		}
 		return $result;
 	}
 
-	public function fetchAll($stmt,$params=array()){
+	public function fetchAll($stmt,$params=array(),$throw_exception=false,$except_code=null,$flatten=false){
+		if(is_array($stmt)) list($stmt,$params) = $stmt;
 		$query = $this->run($stmt,$params);
-		return $query->fetchAll();
+		$result = $query->fetchAll();
+		if(!$result && $throw_exception !== false) throw new Exception($throw_exception,$except_code);
+		if($flatten && is_array($result) && (count($result)>0) && is_array($result[0]) && (count(array_keys($result[0])) == 1)){
+			$col = array_shift(array_keys($result[0]));
+			$arr = array();
+			foreach($result as $row) $arr[] = $row[$col];
+			$result = $arr;
+		}
+		return $result;
 	}
 
 	public function __call($function_name, $parameters) {
 		if(!is_array($parameters)) $parameters = array();
 		return call_user_func_array(array($this->pdo, $function_name), $parameters);
+	}
+	
+	public static function escape($arr=array()){
+		if(!is_array($arr)) return '`'.$arr.'`';
+		foreach($arr as &$f){
+			//join parts of an array into escaped fields
+			if(is_array($f)) $f = '`'.implode('`.`',$f).'`';
+			//escape fields and blow up periods
+			else $f = '`'.implode('`.`',explode('.',$f)).'`';
+		}
+		return $arr;
 	}
 
 }
