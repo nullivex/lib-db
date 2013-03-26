82f27b7f6416809e15171c72f2e34d8eab091517
nullivex <contact@nullivex.com>
2013-03-26 01:26:33 -0700

    added autoloading

--- LSS/Db.php
+++ LSS/Db.php
@@ -17,7 +17,7 @@
  *	You should have received a copy of the 
  *	GNU Lesser General Public License along with OpenLSS.
  *	If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 namespace LSS;
 
 class Db {
